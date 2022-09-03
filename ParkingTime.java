2415ccbea03cfe97c699c251e48900d5db3f5e9f
OHDERIN <76110829+OHDERIN@users.noreply.github.com>
2022-09-03 04:45:41 -0500

    first commit

---
+++ ParkingTime.java
@@ -0,0 +1,29 @@
+//Derin Ergul
+//CSC-1350, Kundu
+import java.util.Scanner;
+public class ParkingTime {
+ public static void main(String[] args) {
+  int numDimesObtained, numMaxParkingMinutes, numNickelsObtained,
+      numPenniesGiven, numPenniesRemaining, numPenniesUsed,
+      numQuartersObtained;
+  Scanner scan = new Scanner(System.in);
+  System.out.print("\nEnter number of pennies given (>=0): ");
+  numPenniesGiven = scan.nextInt();
+  scan.nextLine();
+  numQuartersObtained = numPenniesGiven / 25;
+  numPenniesRemaining = numPenniesGiven % 25;
+  numDimesObtained = numPenniesRemaining / 10;
+  numPenniesRemaining %= 10;
+  numNickelsObtained = numPenniesRemaining / 5;
+  numPenniesRemaining %= 5;
+  numPenniesUsed = (numPenniesGiven - numPenniesRemaining);
+  numMaxParkingMinutes = ((numQuartersObtained * 30) + (numDimesObtained * 11) +
+                          (numNickelsObtained * 5));
+  System.out.println("#(pennies used) = " + numPenniesGiven + ", #(pennies used) = " + 
+                    numQuartersObtained + "x25 + " + numDimesObtained + "x10 + " +
+                    numNickelsObtained + "x5 = " + numPenniesUsed);
+  System.out.println("Maximum #(parking minutes) = " + numQuartersObtained + "x30 + " +
+                    numDimesObtained + "x11 + " + numNickelsObtained + "x5 =" + 
+                    numPenniesUsed);
+    }
+}
