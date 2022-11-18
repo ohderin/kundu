aea82e04df2fd8420308fea7dde4b2a05c861760
OHDERIN <76110829+OHDERIN@users.noreply.github.com>
2022-11-18 02:41:40 -0600

    working!

--- BinaryAddition2.java
+++ BinaryAddition2.java
@@ -2,18 +2,15 @@
 //CSC-1350, Kundu
 
 import java.util.Arrays;
-import java.util.Scanner;
 public class BinaryAddition2
 { final int[] binaryNum, binaryNum2, powerOf2Factors;
 
   public BinaryAddition2(int num, int num2, int numBits)
   { powerOf2Factors = new int[numBits];
     powerOf2Factors[numBits - 1] = 1;
-    for (int i = 1; i < numBits - 2; i++)
-      powerOf2Factors[i] = 2 * powerOf2Factors[i+1];
+    for (int i = numBits - 2; i >= 0; i--)
+        powerOf2Factors[i] = 2 * powerOf2Factors[i + 1];
     int minInt = -powerOf2Factors[0];
-    Scanner scan = new Scanner(System.in);
-    num = scan.nextInt(); num2 = scan.nextInt();
     binaryNum = decimalToBinary(num, numBits, minInt);
     binaryNum2 = decimalToBinary(num2, numBits, minInt);
     System.out.println("binary array-form: " + Arrays.toString(binaryNum) + 
@@ -32,35 +29,40 @@ public class BinaryAddition2
 
   public int[] addBinaryNums()
   { int bitSum, carry = 0, binarySum[] = new int[binaryNum.length];
-    for (int i = binaryNum.length - 1; i >=0; i--)
+    for (int i = binaryNum.length - 1; i >= 1; i--)
     {   bitSum = binaryNum[i] + binaryNum2[i] + carry;
         carry = bitSum / 2;
         binarySum[i] = bitSum % 2;
     }
+    binarySum[0] = (binaryNum[0] + binaryNum2[0] + carry) % 2;
     return (binarySum);
   }
 
   public int binaryToDecimal(int[] binaryNum)
-  { int num = binaryNum[binaryNum.length - 1];
+  { int decNumber = binaryNum[binaryNum.length - 1];
     for (int i = binaryNum.length - 2; i > 0; i--)  
-    {   powerOf2Factors *= 2;
-        num += powerOf2Factors * binaryNum[i];
-    }
-    if (1 == binaryNum[0]) num -= 2 * powerOf2Factors;
-    powerOf2Factors = new int[binaryNum];
-    System.out.println("binary sum-array : " + addBinaryNums() + "; its decimal form " 
-                       + num + "!= num + num2");
+        if (1 == binaryNum[i]) decNumber += powerOf2Factors[i];
+        if (1 == binaryNum[0]) decNumber -= powerOf2Factors[0];
+    return decNumber;
   }
 
   public static void testAddition2()
   { int num, num2, sum, numBits = 6, binarySum[], 
-        integerPairs[][] = {{-15, -20}, {0, 16}, {-32, 17}, {-17, 31}, {-13, -19}},
-        BinaryAddition2, ba2;
-    for (int i = 0; i < integerPairs.length; i--)
-        num = integerPairs[i][i];
+        integerPairs[][] = {{-15, -20}, {0, 16}, {-32, 17}, {-17, 31}, {-13, -19}};
+    BinaryAddition2 ba2;
+    for (int i = 0; i < integerPairs.length; i++)
+    {   num = integerPairs[i][0];
+        num2 = integerPairs[i][1];
         ba2 = new BinaryAddition2(num, num2, numBits);
-        binarySum[] = ba2.addBinaryNums();
+        binarySum = ba2.addBinaryNums();
         sum = ba2.binaryToDecimal(binarySum);
+        if (-32 <= num + num2 && num + num2 <= 32)
+           System.out.println("binary sum-array : " + Arrays.toString(binarySum) + "; its decimal form " 
+                              + sum + " = num + num2");
+        else 
+           System.out.println("binary sum-array : " + Arrays.toString(binarySum) + "; its decimal form " 
+                              + sum + " != num + num2");
+    }
   }
 
   public static void main(String[] args) 
